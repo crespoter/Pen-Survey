15be7eba0ccc7607860a4f4a0dcefd4c6b35494b
crespoter <davidchristiek@gmail.com>
2018-02-19 13:04:12 +0530

    started working on mongo to sql conversion

--- app_sql.js
+++ app_sql.js
@@ -1,9 +1,9 @@
-﻿var express = require('express');
+var express = require('express');
 var bodyparser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID;
 var nodemailer = require('nodemailer');
 var session = require('express-session');
+var mysql = require('mysql');
+
 var transpoter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -11,30 +11,34 @@ var transpoter = nodemailer.createTransport({
         pass: "blanserver"
     }
 });
-
 var app = express();
-MongoClient.connect("mongodb://crespoter:blantest@ds115573.mlab.com:15573/voting-app", (err, database) => {
-    if (err)
-        console.log(err);
-    db = database;
-    app.listen(process.env.PORT|3000, () => {
-        console.log("listening to port " + process.env.PORT | 3000);
-    });
-});
-
 app.set('view engine', 'pug');
 app.use(session({
     secret: "crespotersLittleSecret",
     resave: true,
     saveUninitialized: true
 }));
-app.use(bodyparser.json());      
-app.use(bodyparser.urlencoded({     
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
     extended: true
 }));
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public')); 
-
+//SQL CONNECTION
+var request;
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    port: 3306
+});
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("SQL DATABASE CONNECTED");
+    app.listen(process.env.PORT | 3000, () => {
+        console.log("listening to port " + (process.env.PORT | 3000));
+    });
+});
 
 
 
@@ -50,42 +54,36 @@ app.get('/createSurvey', (req, res) => {
 
 app.get('/register', (req, res) => {
     ssn = req.session;
-    if (ssn.userloggedin)
-    {
+    if (ssn.userloggedin) {
         res.redirect('/');
     }
     res.sendFile("webpages/register.html", { root: 'public' });
 });
 
-
+/*
 app.get('/validateUser', (req, res) => {
-    if (req.query.function == "username-free")
-    {
+    if (req.query.function == "username-free") {
         db.collection("users").find({ name: req.query.username }).toArray((err, results) => {
-            if (results[0] != undefined)
-            {
+            if (results[0] != undefined) {
                 res.json({
                     success: false
                 });
             }
-            else
-            {
+            else {
                 res.json({
                     success: true
                 });
             }
         });
     }
-    if (req.query.function == "check-email")
-    {
+    if (req.query.function == "check-email") {
         db.collection("users").find({ email: req.query.email }).toArray((err, results) => {
             if (results[0] != undefined) {
                 res.json({
                     success: false
                 });
             }
-            else
-            {
+            else {
                 res.json({
                     success: true
                 });
@@ -93,15 +91,14 @@ app.get('/validateUser', (req, res) => {
 
         });
     }
-    if (req.query.function == "register")
-    {
+    if (req.query.function == "register") {
         pendingVisitor = {
             name: req.query.username,
             password: req.query.password,
             email: req.query.email
         };
-       
-        db.collection("pendingUser").save(pendingVisitor);  
+
+        db.collection("pendingUser").save(pendingVisitor);
         retJson = {
             success: true
         }
@@ -114,8 +111,7 @@ app.get('/validateUser', (req, res) => {
         }
 
         transpoter.sendMail(mailOptions, (error, info) => {
-            if (error)
-            {
+            if (error) {
                 retJson.success = false;
                 res.json(retJson);
                 console.log(error);
@@ -153,18 +149,17 @@ app.get('/verify/:userid', (req, res) => {
     }
 });
 
-app.get('/checkLogin',(req, res)=>{
+app.get('/checkLogin', (req, res) => {
     ssn = req.session;
     if (ssn.userLoggedin == undefined || ssn.userLoggedin == false)
-        res.json({ status: false, name:ssn.username });
+        res.json({ status: false, name: ssn.username });
     else
         res.json({ status: true });
 });
 app.post('/login', (req, res) => {
     ssn = req.session;
     db.collection("users").find({ email: req.body.email, password: req.body.password }).toArray((err, results) => {
-        if (results[0] == undefined)
-        {
+        if (results[0] == undefined) {
             var retJson = {
                 status: false
             }
@@ -186,14 +181,13 @@ app.post('/login', (req, res) => {
 
 app.post('/createSurveyAPI', (req, res) => {
     ssn = req.session;
-    if (ssn.userLoggedin == undefined || ssn.userLoggedin == false)
-    {
+    if (ssn.userLoggedin == undefined || ssn.userLoggedin == false) {
         res.send("INVALID REQUEST  UNEXPECTED ERROR");
     }
     var survey = {
         question: req.body.question,
         options: req.body.options,
-        user:ssn.username
+        user: ssn.username
     };
     var votes = [];
     for (var i = 0; i < req.body.options.length; i++) {
@@ -213,7 +207,7 @@ app.get('/surveysAPI/:pagenumber', (req, res) => {
     db.collection("survey").aggregate([{ $project: { _id: "$_id", totalVotes: { $sum: "$votes" }, question: "$question" } }, { $sort: { totalVotes: -1 } }], function (err, data) {
         res.json(data);
     });
-      
+
 });
 
 
@@ -229,8 +223,7 @@ app.get('/mySurveys', (req, res) => {
     if (ssn.userLoggedin == undefined || ssn.userLoggedin == false) {
         res.redirect('/');
     }
-    else
-    {
+    else {
         res.sendFile("webpages/mySurveys.html", { root: 'public' });
     }
 });
@@ -241,10 +234,9 @@ app.get('/mySurveysAPI', (req, res) => {
     if (ssn.userLoggedin == undefined || ssn.userLoggedin == false) {
         res.redirect('/');
     }
-    else
-    {
+    else {
 
-        db.collection("survey").aggregate([{ $project: { _id: "$_id", totalVotes: { $sum: "$votes" }, question: "$question", user: "$user" } }, { $sort: { totalVotes: -1 } },{$match: { user: ssn.username } }], function (err, data) {
+        db.collection("survey").aggregate([{ $project: { _id: "$_id", totalVotes: { $sum: "$votes" }, question: "$question", user: "$user" } }, { $sort: { totalVotes: -1 } }, { $match: { user: ssn.username } }], function (err, data) {
             res.json(data);
         });
     }
@@ -260,12 +252,10 @@ app.get('/survey', (req, res) => {
 app.get('/surveyAPI/:id', (req, res) => {
     id = req.params.id;
     db.collection('survey').find({ _id: ObjectID(id) }).toArray((err, results) => {
-        if (results[0] == undefined)
-        {
+        if (results[0] == undefined) {
             res.send("UNEXPECTED ERROR OCCURED >> PLEASE CONTACT SYSTEM ADMINISTRATOR - Crespoter");
         }
-        else
-        {
+        else {
             res.json(results[0]);
         }
     });
@@ -281,9 +271,8 @@ app.post('/question-submit', (req, res) => {
         user: ssn.username,
         question: req.body.question
     };
-    db.collection("userSurvey").find(userSurvey).toArray((err,data) => {
-        if (data[0] == undefined)
-        {
+    db.collection("userSurvey").find(userSurvey).toArray((err, data) => {
+        if (data[0] == undefined) {
             db.collection('userSurvey').save(userSurvey);
             db.collection('survey').find({ _id: ObjectID(req.body.question) }).toArray((err, results) => {
                 results[0].votes[parseInt(req.body.option) - 1] += 1;
@@ -292,10 +281,9 @@ app.post('/question-submit', (req, res) => {
                 res.redirect("/survey/?id=" + req.body.question);
             });
         }
-        else
-        {
+        else {
             res.send("You have already voted for this survey .");
         }
     });
-    
-});
+
+});*/
