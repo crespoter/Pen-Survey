8039b37c68fee0a5ea3b04b14f6b2c1fa8bb0815
crespoter <davidchristiek@gmail.com>
2018-03-28 15:56:07 +0530

    webside development

--- app_sql.js
+++ app_sql.js
@@ -3,16 +3,18 @@ var bodyparser = require('body-parser');
 var nodemailer = require('nodemailer');
 var session = require('express-session');
 var mysql = require('mysql');
-
+var moment = require('moment');
 var app = express();
 const  pageSize = 10;
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+
 app.use(session({
-    secret: "crespotersLittleSecret",
+    secret: "pensurvey",
     resave: true,
     saveUninitialized: true
 }));
+
 app.use(bodyparser.json());
 
 
@@ -20,6 +22,7 @@ app.use(bodyparser.urlencoded({
     extended: true
 }));
 app.use(express.static(__dirname + '/public'));
+
 
 //SQL CONNECTION
 var request;
@@ -31,6 +34,8 @@ var con = mysql.createConnection({
     database:"pensurvey"
 });
 
+
+
 con.connect(function (err) {
     if (err) throw err;
     console.log("SQL DATABASE CONNECTED");
@@ -39,16 +44,87 @@ con.connect(function (err) {
     });
 });
 
-//WEBSITE GETS
+
 app.get('/', (req, res) => {
     ssn = req.session;
-    res.sendFile("webpages/surveys.html", { root: 'public' });
+    if (ssn.userLoggedin)
+    {
+        var sql = "SELECT * FROM questionnaire WHERE running =1 AND creator_id = " + ssn.userid;
+        con.query(sql, function (err, result, fields) {
+            if (err) {
+                throw err;
+            }
+            res.render("pages/activesurveys.ejs", {questionnaires:result});
+        });
+    }
+    else
+        res.render('pages/login.ejs', { error: '' });
 });
 
+app.post('/', (req, res) => {
+    ssn = req.session;
+    var username = req.body.username;
+    var password = req.body.password;
+    var sql = "SELECT * FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
+    var status = false;
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        if (result.length > 0)  
+        {
+            ssn.userLoggedin = true;
+            ssn.username = result[0].username;
+            ssn.userid = result[0].idUser;
+            res.redirect('/');
+        }
+        else
+        {
+            var sql = "SELECT * FROM user WHERE username = '" + username + "'";
+            con.query(sql, function (err, result, fields) {
+                var message = "ERROR";
+                if (err) throw err;
+                if (result.length > 0)
+                    message = "Password is Incorrect";
+                else
+                    message = "Username doesnt exist";
+                res.render('pages/login.ejs', { error: message });
+            });
+        }
+    });
+});
+
+
+app.get('/expiredsurveys', (req, res) => {
+    ssn = req.session;
+    if (ssn.userLoggedin) {
+        var sql = "SELECT * FROM questionnaire WHERE running =0 AND creator_id = " + ssn.userid;
+        con.query(sql, function (err, result, fields) {
+            if (err) {
+                throw err;
+            }
+            res.render("pages/expiredsurveys.ejs", { questionnaires: result });
+        });
+    }
+    else
+        res.render('pages/login.ejs', { error: '' });
+});
 app.get('/createSurvey', (req, res) => {
     ssn = req.session;
     res.sendFile("webpages/createSurvey.html", { root: 'public' });
-})
+});
+
+app.get('/createDraft', (req, res) => {
+    ssn = req.session;
+    if (ssn.userLoggedin == undefined || ssn.userLoggedin == false)
+        res.redirect('/');
+    else
+    {
+        res.sendFile("webpages/createSurvey.html", { root: 'public' });
+    }
+});
+
+
+
+
 
 app.get('/register', (req, res) => {
     ssn = req.session;
@@ -59,19 +135,22 @@ app.get('/register', (req, res) => {
 });
 
 
-//FOR TEST PURPOSES DELETE THESE
-//TODO
-//-------------------------------------SECURITY LEAK      FOR TEST-------------------------------------------
-app.get('/allusers', (req, res) => {
-    var sql = "SELECT * FROM user";
-    con.query(sql, function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
+app.get('/logout', (req, res) => {
+    ssn = req.session;
+    ssn.userLoggedin = false;
+    res.redirect('/');
 });
-//END SECURITY LEAK
+
+
+
+
+
+
+
 //USER RELATED ##############################################################################################################
 app.post('/login', (req, res) => {
+    ssn = req.session;
+
     var username = req.body.username;
     var password = req.body.password;
     var sql = "SELECT * FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
@@ -90,15 +169,128 @@ app.post('/login', (req, res) => {
             });
         }
         else {
-            res.json({ usernameValid: true, passwordValid: true });
+            ssn.userLoggedin = true;
+            ssn.username = result[0].username;
+            ssn.userid = result[0].idUser;
+            res.json({ usernameValid: true, passwordValid: true,id:result[0].idUser});
         }
     });
 });
 
 
+
+app.get('/checkLogin', (req, res) => {
+    ssn = req.session;
+    if (ssn.userLoggedin == undefined || ssn.userLoggedin == false)
+        res.json({ status: false});
+    else
+        res.json({ status: true, name: ssn.username, id: ssn.userid });
+});
+
 //QUESTIONNAIRE RELATED ###################################################################################################
 
 app.get("/api/getquestionnaire/createdByUser/running/:userid", (req, res) => {
+
+    var sql = "SELECT * FROM questionnaire WHERE running = 1 AND creator_id = " + req.params.userid + " ORDER BY timestamppost DESC";
+    con.query(sql, function (err, result, fields) {
+        if (err) {
+            throw err;
+        }
+        res.json(result);
+    });
+});
+
+
+
+
+
+//TOBE DONE
+app.get("/api/addquestionnaire", (req, res) => {
+    /*var title = req.body.title;
+    var creator_id = req.body.creator_id;
+    var note = req.body.note;
+    var questions = req.body.questions;*/
+
+    var title = req.query.title;
+    var creator_id = req.query.creator_id;
+    var note = req.query.note;
+    var questions = req.query.questions;
+    var timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+    if (title == undefined || creator_id == undefined || note == undefined)
+    {
+        res.json({ message: "invalid parameters", status:false});
+    }
+    else
+    {
+        var sql = "INSERT INTO questionnaire(`idquestionnaire`, `title`, `creator_id`, `running`, `timestamppost`, `note`) VALUES ('0', '" + title + "', " + creator_id + ", '1', '" + timestamp + "', '" + note + "')";
+        con.query(sql, function (err, result, fields)
+        {
+            if (err)
+                throw err;
+            var questionnaireId = result.insertId;
+            if (questions != undefined)
+            {
+                questions.forEach(function (question)
+                {
+                    var question = JSON.parse(question);
+                    if ((question.type==="text" || question.type==="choice") || !question.question_text == undefined)
+                    {
+                        var sql = "INSERT INTO question(idquestion,type,question_text,questionnaire_id) VALUES(0,'" + question.type + "','" + question.question_text + "','" + questionnaireId + "')";
+                        console.log(sql);
+                        con.query(sql, function (err, result, fields) {
+                            if (err) throw err;
+                            var questionId = result.insertId;
+                            if (question.choices != undefined)
+                            {
+                                question.choices.forEach(function (choice) {
+                                    var sql = "INSERT INTO choices(idchoices,choice,question_id) VALUES(0,'" + choice.choice + "'," + qid + ")";
+                                    con.query(sql, function (err, result, fields) {
+                                        if (err) throw err;
+                                    });
+                                }.bind({ qid: questionId}));
+                            }
+                        }.bind({ question: question }));
+                        res.json({ success: true });
+                    }
+                    else
+                    {
+                        res.json({ success: false });
+                    }
+                });
+            }
+        });
+        
+    }
+});
+
+
+
+
+app.get("/api/addresponse/:questionid", (req, res) => {
+    var question_id = req.params.questionid;
+    var userid = req.query.userid;
+    var response = req.query.response;
+    var sql = "SELECT * FROM response WHERE user_id = " + userid + " AND question_id =" + question_id;
+
+    con.query(sql, function (err, result, fields) {
+        if (err) {
+            throw err;
+        }
+        if (result.length > 0)
+            res.json({ error: "Response already submitted" });
+        else {
+            var sql = "INSERT INTO response(idresponse,response,question_id,user_id) VALUES(0,'" + response + "'," + question_id + "," + userid + ")";
+            con.query(sql, function (err, result, fields) {
+                if (err) {
+                    throw err;
+                }
+                res.json({ error: "none" });
+            });
+        }
+    });
+});
+
+app.get("/api/getquestionnaire/createdbyuser/expired/:userid", (req, res) => {
 
     var sql = "SELECT * FROM questionnaire WHERE running = 0 AND creator_id = " + req.params.userid;
     con.query(sql, function (err, result, fields) {
@@ -108,19 +300,10 @@ app.get("/api/getquestionnaire/createdByUser/running/:userid", (req, res) => {
         res.json(result);
     });
 });
-app.get("/api/getquestionnaire/createdbyuser/expired/:userid", (req, res) => {
+app.get("/api/getquestionnaire/foruser/:userid", (req, res) => {    // BUGGY ____DONT USE
 
-    var sql = "SELECT * FROM questionnaire WHERE running = 1 AND creator_id = " + req.params.userid;
-    con.query(sql, function (err, result, fields) {
-        if (err) {
-            throw err;
-        }
-        res.json(result);
-    });
-});
-app.get("/api/getquestionnaire/foruser/:userid", (req, res) => {
+    var sql = "SELECT questionnaire.idquestionnaire,questionnaire.title,questionnaire.creator_id,questionnaire.running,questionnaire.note,questionnaire.timestamppost,user.username FROM questionnaire JOIN question JOIN response JOIN user WHERE questionnaire.idquestionnaire = question.questionnaire_id AND response.question_id = question.idquestion AND response.user_id = user.idUser AND  user.idUser = " + req.params.userid;
 
-    var sql = "SELECT idquestionnaire,questionnaire.title,questionnaire.creator_id,questionnaire.running AS idquestionnaire, title, creator_id, running FROM questionnaire JOIN question JOIN response WHERE questionnaire.idquestionnaire = question.questionnaire_id AND response.question_id = question.idquestion AND response.question_id = " + req.params.userid;
     con.query(sql, function (err, result, fields) {
         if (err) {
             throw err;
@@ -150,6 +333,17 @@ app.get("/api/getchoices/:id", (req, res) => {
 //PERMISSIONS #############################################################################################################
 app.get("/api/getpermissions/:userid", (req, res) => {
     var sql = "SELECT * FROM permissions WHERE user_id = " + req.params.userid;
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+
+//DRAFTS####################################################################################################################
+
+app.get("/api/getdrafts/:userid", (req, res) => {
+    var sql = "SELECT * FROM drafts WHERE creator_id = " + req.params.userid;
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
         res.json(result);
